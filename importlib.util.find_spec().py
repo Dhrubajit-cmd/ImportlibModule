1fe3f1ddafec3c3d1f4603fd96802a3382a0641a
Dhrubajit Chakravarty <dhrubajitchakravorty@gmail.com>
2024-09-19 20:39:48 +0530

    Commit

---
+++ importlib.util.find_spec().py
@@ -0,0 +1,12 @@
+# importlib allows you to inspect a module's specifications without importing it. This provides metadata about the module(like its name, location, etc.)
+
+import importlib.util 
+
+# Find the specification of a module : 
+ 
+spec = importlib.util.find_spec('json')
+
+# Get details form the specification : 
+
+print(f"Module name : {spec.name} \n")
+print(f"Moduel origin : {spec.origin} \n")
